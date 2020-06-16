9e310e4f99d08cd4ce475ee3a95450f4330b7ce3
kevin Cotrina <50785733+kcotrinam92@users.noreply.github.com>
2020-06-16 04:51:13 -0500

    computer choice is complete and shown on DOMd

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('HEllo World')
+"use strict";
+const options = ["rock", "paper", "scissors"]
+const imagesPlayerOption = ["img/right-rock.png", "img/right-paper.png", "img/right-scissors.png"],
+        imagesComputerOption = ["img/left-rock.png", "img/left-paper.png", "img/left-scissors.png"]
+
+const pcImageChoice = document.querySelector("#pc-choice")
+
+
+
+
+
+
+
+const computerRandomChoice = options[Math.floor(Math.random() * options.length)]
+
+
+const showComputerChoice = () => {
+    switch (computerRandomChoice) {
+        case 'rock':
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
+            break;
+        case 'paper':
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
+            break
+        default:
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
+            break;
+    }
+        
+    
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    showComputerChoice();
+})
