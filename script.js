253a32994d1216c9dab65ff167709b63944e8f1b
kevin Cotrina <50785733+kcotrinam92@users.noreply.github.com>
2020-06-16 04:56:37 -0500

    computer choice complted

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 "use strict";
 const options = ["rock", "paper", "scissors"]
 const imagesPlayerOption = ["img/right-rock.png", "img/right-paper.png", "img/right-scissors.png"],
-        imagesComputerOption = ["img/left-rock.png", "img/left-paper.png", "img/left-scissors.png"]
+        imagesComputerOption = ["img/left-rock.png", "img/left-paper.png", "img/left-scissors.png"];
 
-const pcImageChoice = document.querySelector("#pc-choice")
-
-
+const pcImageChoice = document.querySelector("#pc-choice");
 
 
 
 
 
-const computerRandomChoice = options[Math.floor(Math.random() * options.length)]
+
+
+const computerRandomChoice = options[Math.floor(Math.random() * options.length)];
 
 
 const showComputerChoice = () => {
     switch (computerRandomChoice) {
         case 'rock':
-            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
             break;
         case 'paper':
-            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
-            break
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
+            break;
         default:
-            pcImageChoice.src = `img/left-${computerRandomChoice}.png`
+            pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
             break;
     }
         
