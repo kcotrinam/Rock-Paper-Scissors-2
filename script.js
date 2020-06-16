6087f76c15029d2fe337c71caa50caff16c42663
kevin Cotrina <50785733+kcotrinam92@users.noreply.github.com>
2020-06-16 06:09:09 -0500

    This change show picture chosen by the player

--- script.js
+++ script.js
@@ -2,28 +2,45 @@
 const options = ["rock", "paper", "scissors"]
 const imagesPlayerOption = ["img/right-rock.png", "img/right-paper.png", "img/right-scissors.png"],
         imagesComputerOption = ["img/left-rock.png", "img/left-paper.png", "img/left-scissors.png"];
+const playerOptions = [...document.querySelectorAll('.img-item')]
+const playerChoiceImage = document.querySelector("#player-choice")
 
 const pcImageChoice = document.querySelector("#pc-choice");
 
 
+    
+let playerChoice
+let computerChoice
+
+playerOptions.map(option => {
+    option.addEventListener('click', e => {
+        playerChoice = e.target.alt
+        playerChoiceImage.src = `img/right-${playerChoice}.png`
+        showComputerChoice();
+    })
+    
+})
 
 
 
 
 
-const computerRandomChoice = options[Math.floor(Math.random() * options.length)];
 
 
 const showComputerChoice = () => {
+    const computerRandomChoice = options[Math.floor(Math.random() * options.length)];
     switch (computerRandomChoice) {
         case 'rock':
             pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
+            computerChoice = computerRandomChoice
             break;
         case 'paper':
             pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
+            computerChoice = computerRandomChoice
             break;
         default:
             pcImageChoice.src = `img/left-${computerRandomChoice}.png`;
+            computerChoice = computerRandomChoice
             break;
     }
         
@@ -31,5 +48,5 @@ const showComputerChoice = () => {
 }
 
 document.addEventListener('DOMContentLoaded', () =>{
-    showComputerChoice();
+
 })
